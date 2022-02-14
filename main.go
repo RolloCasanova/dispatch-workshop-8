5164c0b08b3baaef38510e0d649e3920b04de258
Roboam Casanova Castro <zr.rollo@gmail.com>
2022-02-14 16:16:01 -0600

    Use the interface{} hack

--- main.go
+++ main.go
@@ -7,19 +7,20 @@ func main() {
 	int64Value := int64(20)
 	float64Value := float64(30)
 
-	fmt.Println(DescribeInt(intValue))
-	fmt.Println(DescribeInt64(int64Value))
-	fmt.Println(DescribeFloat64(float64Value))
+	fmt.Println(DescribeVar(intValue))
+	fmt.Println(DescribeVar(int64Value))
+	fmt.Println(DescribeVar(float64Value))
 }
 
-func DescribeInt(v int) string {
-	return fmt.Sprintf("Param 'v' is of type int and its value is: %d", v)
-}
-
-func DescribeInt64(v int64) string {
-	return fmt.Sprintf("Param 'v' is of type int64 and its value is: %d", v)
-}
-
-func DescribeFloat64(v float64) string {
-	return fmt.Sprintf("Param 'v' is of type float64 and its value is: %f", v)
+func DescribeVar(v interface{}) string {
+	switch v.(type) {
+	case int:
+		return fmt.Sprintf("Param 'v' is of type int and its value is: %d", v)
+	case int64:
+		return fmt.Sprintf("Param 'v' is of type int64 and its value is: %d", v)
+	case float64:
+		return fmt.Sprintf("Param 'v' is of type float64 and its value is: %f", v)
+	default: // <- unsupported type
+		return fmt.Sprintf("[Unsupported type] Param 'v' is of type %T and its value is: %v", v, v)
+	}
 }
