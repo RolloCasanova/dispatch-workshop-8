e45e8b3a060ddbc5bf7951c8a191a046326edfdf
Roboam Casanova Castro <zr.rollo@gmail.com>
2022-02-14 16:27:03 -0600

    Say hello to Generics

--- main.go
+++ main.go
@@ -7,20 +7,19 @@ func main() {
 	int64Value := int64(20)
 	float64Value := float64(30)
 
-	fmt.Println(DescribeVar(intValue))
-	fmt.Println(DescribeVar(int64Value))
-	fmt.Println(DescribeVar(float64Value))
+	fmt.Println(DescribeVar[int](intValue))
+	fmt.Println(DescribeVar[int64](int64Value))
+	fmt.Println(DescribeVar[float64](float64Value))
+
+	// // Type argument is not mandatory
+	// fmt.Println(DescribeVar(intValue))
+	// fmt.Println(DescribeVar(int64Value))
+	// fmt.Println(DescribeVar(float64Value))
+
+	// // A type that is not in the list of generic types  wont compile
+	// fmt.Println(DescribeVar("40"))
 }
 
-func DescribeVar(v interface{}) string {
-	switch v.(type) {
-	case int:
-		return fmt.Sprintf("Param 'v' is of type int and its value is: %d", v)
-	case int64:
-		return fmt.Sprintf("Param 'v' is of type int64 and its value is: %d", v)
-	case float64:
-		return fmt.Sprintf("Param 'v' is of type float64 and its value is: %f", v)
-	default: // <- unsupported type
-		return fmt.Sprintf("[Unsupported type] Param 'v' is of type %T and its value is: %v", v, v)
-	}
+func DescribeVar[T int | int64 | float64](v T) string {
+	return fmt.Sprintf("Param 'v' is of type %T and its value is: %v", v, v)
 }
