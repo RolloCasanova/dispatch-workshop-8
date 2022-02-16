9e504b725bffe52081bed7ecd55c9d1eb63fd8b6
Roboam Casanova Castro <zr.rollo@gmail.com>
2022-02-16 14:15:42 -0600

    Fix the error - add log to see the runes of string and reversed string

--- stringz/reverse.go
+++ stringz/reverse.go
@@ -1,7 +1,11 @@
 package stringz
 
+import "log"
+
 func Reverse(s string) string {
+	log.Printf("input: %q\n", s)
 	r := []rune(s)
+	log.Printf("runes: %q\n", r)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
