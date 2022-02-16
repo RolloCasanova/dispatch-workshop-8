bd0c967d10d349b5bb912e2b25ed447d052ce9d9
Roboam Casanova Castro <zr.rollo@gmail.com>
2022-02-16 00:21:02 -0600

    Multiple types - interface - any

--- main.go
+++ main.go
@@ -2,24 +2,70 @@ package main
 
 import "fmt"
 
+// type myCustomInt int
+// type myOtherCustomInt int
+
+// type customInts interface {
+// 	myCustomInt | myOtherCustomInt
+// }
+
 func main() {
 	intValue := 10
 	int64Value := int64(20)
 	float64Value := float64(30)
+	stringValue := "40"
+	arrayValue := []rune{'a', 'b', 'c'}
+	boolValue := true
+	mapValue := map[string]string{"k1": "v1"}
+	structValue := struct{}{}
 
-	fmt.Println(DescribeVar[int](intValue))
-	fmt.Println(DescribeVar[int64](int64Value))
-	fmt.Println(DescribeVar[float64](float64Value))
+	fmt.Println(DescribeVar(intValue))
+	fmt.Println(DescribeVar(int64Value))
+	fmt.Println(DescribeVar(float64Value))
+	fmt.Println(DescribeVar(stringValue))
+	fmt.Println(DescribeVar(arrayValue))
+	fmt.Println(DescribeVar(boolValue))
+	fmt.Println(DescribeVar(mapValue))
+	fmt.Println(DescribeVar(structValue))
 
-	// // Type argument is not mandatory
-	// fmt.Println(DescribeVar(intValue))
-	// fmt.Println(DescribeVar(int64Value))
-	// fmt.Println(DescribeVar(float64Value))
+	// // for tilde operator
+	// var customInt myCustomInt = 100
+	// var otherCustomInt myOtherCustomInt = 200
 
-	// // A type that is not in the list of generic types  wont compile
-	// fmt.Println(DescribeVar("40"))
+	// fmt.Println(DescribeVar(customInt))
+	// fmt.Println(DescribeVar(otherCustomInt))
 }
 
-func DescribeVar[T int | int64 | float64](v T) string {
+// Hardcode the types of the variable
+func DescribeVar[T int | int64 | float64 | string | []rune | bool | map[string]string | struct{}](v T) string {
 	return fmt.Sprintf("Param 'v' is of type %T and its value is: %v", v, v)
 }
+
+// // Interface approach
+// type myValues interface {
+// 	int | int64 | float64 |
+// 	string | []rune | bool |
+// 	map[string]string | struct{}
+// }
+
+// func DescribeVar[T myValues](v T) string {
+// 	return fmt.Sprintf("Param 'v' is of type %T and its value is: %v", v, v)
+// }
+
+// // Any (interface{}) apprach
+// func DescribeVar[T any](v T) string {
+// 	return fmt.Sprintf("Param 'v' is of type %T and its value is: %v", v, v)
+// }
+
+// func DescribeVar[T interface{}](v T) string {
+// 	return fmt.Sprintf("Param 'v' is of type %T and its value is: %v", v, v)
+// }
+
+// // Custom types (tilde operator)
+// func DescribeVar[T int | myCustomInt | myOtherCustomInt](v T) string {
+// 	return fmt.Sprintf("Param 'v' is of type %T and its value is: %v", v, v)
+// }
+
+// func DescribeVar[T ~int](v T) string {
+// 	return fmt.Sprintf("Param 'v' is of type %T and its value is: %v", v, v)
+// }
